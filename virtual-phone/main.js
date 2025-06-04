a2de793ae516297cec556d444b009da2bdfa096a
igorcampos90 <igorcampos708@gmail.com>
2025-06-03 23:14:44 -0300

    adiciona javascript

---
+++ virtual-phone/main.js
@@ -0,0 +1,24 @@
+const display = document.querySelector('input[type=tel]');
+
+const teclas = 
+    document.querySelectorAll('input[type=button]');
+
+const limpar = document.querySelector('.icon');
+
+limpar.onclick = () => {
+    const opcao = confirm('Deseja mesmo limpar?');
+    if (opcao) {
+        display.value = "";
+    }    
+}
+
+function valorTeclaToDisplay (i) {
+    display.value += teclas[i].value;
+}
+
+
+for(let i = 0; i < teclas.length; i++){
+    teclas[i].onclick = () => {
+        valorTeclaToDisplay(i);
+    }
+}
